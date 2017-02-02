345e235e854f38f5a45368e4d85dfda2e80dd5ef
caleb <caleb.ray.williams@gmail.com>
2017-02-02 01:19:49 -0600

    TwtichAPI
    Twitch api queried

---
+++ InterfaceTests/AppConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTests
+{
+    public class AppConfig
+    {
+
+        public Dictionary<string, string> Tokens { get; set; }
+        public AppConfig()
+        {
+            Tokens = new Dictionary<string, string>();
+            Tokens.Add("twitch-api", "key");
+        }
+    }
+}
